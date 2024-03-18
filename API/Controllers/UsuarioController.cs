45f53361df66d7a37e10a4598e8527da83db7fcc
MauroCR52 <mauluna52@gmail.com>
2024-03-18 16:21:48 -0600

    funcion buscar chef o admin

---
+++ API/Controllers/UsuarioController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("usuario")]
+    public class UsuarioController
+    {
+
+        [HttpGet]
+        [Route("encontrarCorreoPasswd")]
+        public dynamic encontrarCorreoPasswd(string correo, string password)
+        {
+            string json = System.IO.File.ReadAllText("database.json");
+            var data = JsonConvert.DeserializeObject<dynamic>(json);
+
+            // Buscar entre los administradores
+            foreach (var admin in data.administradores)
+            {
+                if (admin.correo == correo && admin.contraseña == password)
+                {
+                    return new { tipo = "administrador", datos = admin };
+                }
+            }
+
+            // Buscar entre los chefs
+            foreach (var chef in data.chefs)
+            {
+                if (chef.correo == correo && chef.contraseña == password)
+                {
+                    return new { tipo = "chef", datos = chef };
+                }
+            }
+
+            return null;
+        }
+    }
+}
