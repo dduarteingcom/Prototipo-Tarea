f4594b813bfc27626a3bf84bbf61a1f7ecae6030
MauroCR52 <mauluna52@gmail.com>
2024-03-18 20:32:11 -0600

    Funcion encontrar pedidos activos

---
+++ API/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("admin")]
+
+    public class AdminController : ControllerBase
+    {
+        [HttpGet]
+        [Route("obtenerPedidosActivos")]
+        public IActionResult ObtenerPedidosActivos()
+        {
+            // Leer el archivo json
+            var json = System.IO.File.ReadAllText("database.json");
+            var data = JsonConvert.DeserializeObject<Root>(json);
+
+            // Filtrar los pedidos cuyo estado sea true
+            var pedidosActivos = data.pedidos.Where(p => p.estado == true).ToList();
+
+            if (pedidosActivos.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(pedidosActivos);
+        }
+    }
+}
