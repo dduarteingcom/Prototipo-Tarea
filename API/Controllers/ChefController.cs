d1e7d93bb740aabdb41524a36d16028e046746ea
MauroCR52 <mauluna52@gmail.com>
2024-03-18 20:42:09 -0600

    Funcion encontrar pedidos activos por chef

---
+++ API/Controllers/ChefController.cs
@@ -0,0 +1,33 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("chef")]
+
+
+    public class ChefController : ControllerBase
+    {
+        [HttpGet]
+        [Route("obtenerPedidosActivosChef")]
+        public IActionResult ObtenerPedidosActivosChef(string correo)
+        {
+            // Leer el archivo json
+            var json = System.IO.File.ReadAllText("database.json");
+            var data = JsonConvert.DeserializeObject<Root>(json);
+
+            // Filtrar los pedidos cuyo estado sea true
+            var pedidosActivos = data.pedidos.Where(p => p.estado == true && p.chef == correo).ToList();
+
+            if (pedidosActivos.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(pedidosActivos);
+        }
+
+    }
+}
