9aefef138c54246c23c72c0b20796580ae8d236d
dduarteingcom <daduarte@estudiantec.cr>
2024-03-20 11:03:39 -0600

    Commit previo a merge con main 2.0

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc664b4f9ff67ddc429409e54119a66cfeb96e35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45cc882d2342346014e4833e9e72828281634dfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
