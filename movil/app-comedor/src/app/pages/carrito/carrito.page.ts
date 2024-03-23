e1fd8167bf552d886df2adf1aea44e34350fe052
FelipeJU22 <felipejimenezu03@gmail.com>
2024-03-22 18:50:41 -0600

    Solicitar pedido a la API

--- movil/app-comedor/src/app/pages/carrito/carrito.page.ts
+++ movil/app-comedor/src/app/pages/carrito/carrito.page.ts
@@ -6,20 +6,30 @@ import { Router } from '@angular/router';
 import { CarritoService } from 'src/app/services/carrito.service';
 import { IDish } from '../interfaces/plato.model';
 import { FacturaService } from 'src/app/services/factura.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CONFIG } from 'src/config/config';
+import { CheckUserService } from 'src/app/services/check-user.service';
+
+
 
 @Component({
   selector: 'app-carrito',
   templateUrl: './carrito.page.html',
   styleUrls: ['./carrito.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, HttpClientModule]
 })
 export class CarritoPage implements OnInit {
   pedidos: IDish[] =[];
+  idplatos: number[] = [];
+  readonly APIUrl = CONFIG.apiUrl + "pedido/"
   constructor(
     private _router : Router,
     private _carritoService: CarritoService,
     private _facturaService: FacturaService,
+    private _httpClient: HttpClient,
+    private _checkUser: CheckUserService,
+
   ){
 
   }
@@ -31,12 +41,21 @@ export class CarritoPage implements OnInit {
     this._router.navigate(['/home'])
   }
   goPedido(){
+    for(let idplato of this.pedidos){
+      this.idplatos.push(idplato.id);
+    }
+    let pedidoSolicitado = {
+      cliente: this._checkUser.getCedula(),
+      platos: this.idplatos
+    }
+    this._httpClient.post(this.APIUrl + 'agregarPedido', pedidoSolicitado).subscribe();
     this._facturaService.setHora();
     if(this.pedidos.length>0){
       this._facturaService.setFecha();
       this._facturaService.setHora();
       this._router.navigate(['/pedido']);
     }
+
 
   }
   removeItem(pedido:string){
