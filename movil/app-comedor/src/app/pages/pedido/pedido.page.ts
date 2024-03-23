ace6be79c94e95afa496bbd7e423b0271a675327
FelipeJU22 <felipejimenezu03@gmail.com>
2024-03-22 18:13:03 -0600

    Tiempo estimado

--- movil/app-comedor/src/app/pages/pedido/pedido.page.ts
+++ movil/app-comedor/src/app/pages/pedido/pedido.page.ts
@@ -16,12 +16,16 @@ import { CheckUserService } from 'src/app/services/check-user.service';
   imports: [IonicModule, CommonModule, FormsModule]
 })
 export class PedidoPage implements OnInit {
+  intervalId: any;
   pedidos: IDish[] =[];
   montoTotal: number = 0;
   fecha: string = "";
   hora: string = "";
   id: number = 0;
   nombre: string = "";
+  tiempoRestante: number = 2;
+  mostrarDiv: boolean = false;
+  textoRestante: string = "Faltan " + this.tiempoRestante + " minutos para que se complete tu pedido.";
   constructor(
     private _router: Router,
     private _carritoService: CarritoService,
@@ -30,12 +34,33 @@ export class PedidoPage implements OnInit {
     ) { }
 
   ngOnInit() {
+    this.setDuracion();
     this.pedidos=this._carritoService.getPedidos();
     this.getMontoTotal();
     this.fecha = this._facturaService.getFecha();
     this.hora = this._facturaService.getHora();
     this.id = Math.floor(Math.random() * (1000000 - 99999 + 1)) + 99999;
     this.nombre = this._checkUser.geNombre();
+    this.intervalId = setInterval(() => {
+      console.log(this.tiempoRestante);
+      this.tiempoRestante--;
+      if(this.tiempoRestante > 1){
+        this.textoRestante = "Faltan " + this.tiempoRestante + " minutos para que se complete tu pedido."
+      }
+      if(this.tiempoRestante == 1){
+        this.textoRestante = "Falta " + this.tiempoRestante + " minuto para que se complete tu pedido."
+      }
+      if(this.tiempoRestante < 1){
+        this.textoRestante = "Tu pedido se ha completado."
+        this.mostrarDiv = true;
+        clearInterval(this.intervalId);
+      }
+    }, 6000);
+  }
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
   }
 
   goBack(){
@@ -50,5 +75,12 @@ export class PedidoPage implements OnInit {
   goFeedback(){
     this._router.navigate(['/feedback'])
   }
+
+  setDuracion(){
+    for (let pedido of this.pedidos){
+      this.tiempoRestante += pedido.duracion;
+    }
+  }
+  
 
 }
